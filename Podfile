48e4422a8670fd8b56269f454ea70738fe5f06b8
Paul Escarcena <paul.escarcena@gmail.com>
2018-05-28 23:57:44 -0500

    primer commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+# platform :ios, '9.0'
+
+target 'lyfe' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+platform :ios, '9.0'
+  use_frameworks!
+
+pod 'EMSpinnerButton'
+
+pod 'Material', '~> 2.0'
+  # Pods for lyfe
+
+end
---
+++ lyfe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lyfe
+//
+//  Created by Paul Escarcena on 5/28/18.
+//  Copyright © 2018 Paul Escarcena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var btns: [LyfeRadioButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        Thread.sleep(forTimeInterval: 3.0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
